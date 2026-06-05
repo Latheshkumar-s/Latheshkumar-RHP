905d30645f8aaf50a24f20ccc6562d1be7fdefa0
Latheshkumar-s <lathesh.2006@gmail.com>
2026-06-05 19:28:59 +0530

    Create digits_1_to_9.c

---
+++ digits_1_to_9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[1000001];
+    scanf("%s", s);
+
+    int m = 0;
+
+    for(int i = 0; s[i]; i++)
+    {
+        if(s[i] >= '1' && s[i] <= '9')
+            m |= (1 << (s[i] - '1'));
+    }
+
+    if(m == ((1 << 9) - 1))
+        printf("Yes");
+    else
+        printf("No");
+
+    return 0;
+}
